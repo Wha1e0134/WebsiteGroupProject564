d8479128fc1337e9b5af6f68325b257cea69a634
wy69-stack <6705140049@siam.edu>
2026-02-23 22:34:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function toggleMode() {
     document.body.classList.toggle("dark");
 }
+// Get form
+var form = document.getElementById("contactForm");
+
+form.onsubmit = function(event) {
+
+    event.preventDefault(); // Stop page refresh
+
+    var name = document.getElementById("name").value;
+
+    var message = document.getElementById("successMessage");
+
+    message.innerHTML = "Thank you " + name + "! Your message has been sent successfully.";
+
+    form.reset(); // Clear form
+};
